8b96eadbb48caf5feff7a857c7c17476291a7327
ertanuzunoglu <ertanuzunogl@gmail.com>
2023-02-09 13:55:51 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fm = function () {
-  console.log("Function working");
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const myLogger = (req, res, next) => {
+    console.log('ertan');
+    next();
 };
+
+//middlewares
+app.use(express.static('public'));
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './temp/index.html'));
+});
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda çalışıyor.`);
+});
